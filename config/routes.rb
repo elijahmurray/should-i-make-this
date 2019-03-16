d4fe3921ed3cf1c9b9d95a2b2816d0fa86de7fd2
Elijah Murray <elijahmurray@gmail.com>
2019-03-16 19:28:50 -0400

    setup new home page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get 'page/index'
   devise_for :users
 
-  root 'page#index'
+  root 'products#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
