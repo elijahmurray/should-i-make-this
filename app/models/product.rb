ad13cea8f6f6d0da65812482b3a9d3d1d419fe95
Elijah Murray <elijahmurray@gmail.com>
2019-03-16 19:12:10 -0400

    setup auth for create

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,12 +1,12 @@
 class Product < ApplicationRecord
-  TYPES = %w(mobile_app, website, browser_extension, physical_product, voice app)
-  STAGES = [
-    "Idea", 
-    "Wireframes",
-    "Mockups", 
-    "Clickable Prototype",
-    "Development in Progress",
-    "Launch Ready",
-    "Launched"
-    ]
+  TYPES = %w(mobile_app website browser_extension physical_product voice_app)
+  STAGES = %w(
+    Idea
+    Wireframes
+    Mockups
+    Clickable_Prototype
+    Development_in_Progress
+    Launch_Ready
+    Launched
+  )
 end
